8266578dd6151f17238cebf6f0c7a12eae46aec0
ezuyeva <lisa.zuyeva@web.de>
2020-10-22 13:20:55 +0100

    Exception Handling for SQL connector

--- data-generator/sqlconnect.py
+++ data-generator/sqlconnect.py
@@ -1,8 +1,21 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-cnx = mysql.connector.connect(host = 'localhost', database = 'db_grad_cs_1917', user = 'root', password='ppp')
-cursor = cnx.cursor()
+try:
+    cnx = mysql.connector.connect(host = 'localhost', 
+                                  database = 'db_grad_cs_1917', 
+                                  user = 'root', 
+                                  password='ppp')
+    cursor = cnx.cursor()                              
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cnx.close()                                                  
 
 create_newtable = ("CREATE TABLE deals_stream ("
                         "deal_id int"
